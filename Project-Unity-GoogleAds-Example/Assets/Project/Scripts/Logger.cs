e7477389e9cf539381ab47d3d99b359870d9b446
ChrisT.Jie <79248930+ChrisTJie@users.noreply.github.com>
2021-03-15 14:08:03 +0800

    Project settings changes

---
+++ Project-Unity-GoogleAds-Example/Assets/Project/Scripts/Logger.cs
@@ -0,0 +1,25 @@
+﻿namespace CTJ
+{
+    public static class Logger
+    {
+        private const string SYMBOL = "LOGGER";
+
+        [System.Diagnostics.Conditional(SYMBOL)]
+        public static void Log(object _message) { UnityEngine.Debug.Log(_message); }
+
+        [System.Diagnostics.Conditional(SYMBOL)]
+        public static void LogFormat(string _message, params object[] _args) { UnityEngine.Debug.LogFormat(_message, _args); }
+
+        [System.Diagnostics.Conditional(SYMBOL)]
+        public static void LogWarning(object _message) { UnityEngine.Debug.LogWarning(_message); }
+
+        [System.Diagnostics.Conditional(SYMBOL)]
+        public static void LogWarningFormat(string _message, params object[] _args) { UnityEngine.Debug.LogWarningFormat(_message, _args); }
+
+        [System.Diagnostics.Conditional(SYMBOL)]
+        public static void LogError(object _message) { UnityEngine.Debug.LogError(_message); }
+
+        [System.Diagnostics.Conditional(SYMBOL)]
+        public static void LogErrorFormat(string _message, params object[] _args) { UnityEngine.Debug.LogErrorFormat(_message, _args); }
+    }
+}
