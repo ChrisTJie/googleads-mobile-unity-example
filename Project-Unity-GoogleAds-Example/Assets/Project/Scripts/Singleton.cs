c0cdd3ec3f96b23718b9586ad094e00359d5c72b
ChrisT.Jie <79248930+ChrisTJie@users.noreply.github.com>
2021-03-22 18:21:10 +0800

    Added custom event

---
+++ Project-Unity-GoogleAds-Example/Assets/Project/Scripts/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace CTJ
+{
+    public class Singleton<T> : MonoBehaviour where T : class, new()
+    {
+        private static T _Instance;
+
+        private static readonly object _Locker = new object();
+
+        public static T Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    lock (_Locker)
+                    {
+                        if (_Instance == null)
+                        {
+                            _Instance = new T();
+                            Logger.LogFormat("創建單利對象：{0}", typeof(T).Name);
+                        }
+                    }
+                }
+                return _Instance;
+            }
+        }
+    }
+}
