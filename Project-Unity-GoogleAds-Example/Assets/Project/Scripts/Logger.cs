ffdc1ee6f02478ff2d0a21396a8ad763c4c42313
ChrisT.Jie <79248930+ChrisTJie@users.noreply.github.com>
2021-04-05 19:53:26 +0800

    Change List 4 - Improvment Logger Script

--- Project-Unity-GoogleAds-Example/Assets/Project/Scripts/Logger.cs
+++ Project-Unity-GoogleAds-Example/Assets/Project/Scripts/Logger.cs
@@ -5,21 +5,21 @@
         private const string SYMBOL = "LOGGER";
 
         [System.Diagnostics.Conditional(SYMBOL)]
-        public static void Log(object _message) { UnityEngine.Debug.Log(_message); }
+        internal static void Log(object _message) { UnityEngine.Debug.Log(_message); }
 
         [System.Diagnostics.Conditional(SYMBOL)]
-        public static void LogFormat(string _message, params object[] _args) { UnityEngine.Debug.LogFormat(_message, _args); }
+        internal static void LogFormat(string _message, params object[] _args) { UnityEngine.Debug.LogFormat(_message, _args); }
 
         [System.Diagnostics.Conditional(SYMBOL)]
-        public static void LogWarning(object _message) { UnityEngine.Debug.LogWarning(_message); }
+        internal static void LogWarning(object _message) { UnityEngine.Debug.LogWarning(_message); }
 
         [System.Diagnostics.Conditional(SYMBOL)]
-        public static void LogWarningFormat(string _message, params object[] _args) { UnityEngine.Debug.LogWarningFormat(_message, _args); }
+        internal static void LogWarningFormat(string _message, params object[] _args) { UnityEngine.Debug.LogWarningFormat(_message, _args); }
 
         [System.Diagnostics.Conditional(SYMBOL)]
-        public static void LogError(object _message) { UnityEngine.Debug.LogError(_message); }
+        internal static void LogError(object _message) { UnityEngine.Debug.LogError(_message); }
 
         [System.Diagnostics.Conditional(SYMBOL)]
-        public static void LogErrorFormat(string _message, params object[] _args) { UnityEngine.Debug.LogErrorFormat(_message, _args); }
+        internal static void LogErrorFormat(string _message, params object[] _args) { UnityEngine.Debug.LogErrorFormat(_message, _args); }
     }
 }
