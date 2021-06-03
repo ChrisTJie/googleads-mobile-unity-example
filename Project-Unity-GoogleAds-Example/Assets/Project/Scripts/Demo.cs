71f268a01591750a9689c7569e2a3d33023f70f3
ChrisT.Jie <79248930+ChrisTJie@users.noreply.github.com>
2021-06-03 15:14:20 +0800

    Change List 25

--- Project-Unity-GoogleAds-Example/Assets/Project/Scripts/Demo.cs
+++ Project-Unity-GoogleAds-Example/Assets/Project/Scripts/Demo.cs
@@ -1,39 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Demo : MonoBehaviour
 {
-    [SerializeField] private GameObject _NativeObject;
-    [SerializeField] private GameObject _AdChoicesLogo;
-    [SerializeField] private GameObject _Advertiser;
-    [SerializeField] private GameObject _Body;
-    [SerializeField] private GameObject _CallToAction;
-    [SerializeField] private GameObject _Headline;
-    [SerializeField] private GameObject _Icon;
-
     private void Start()
     {
         CTJ.Logger.Log(CTJ.Ads.Instance.GetGDPRApplicable);
-        _NativeObject.SetActive(false);
-        CTJ.Ads.Instance.EventAddListener_NativeInitialize(DemoNativeInitialize);
-    }
-
-    private void DemoNativeInitialize()
-    {
-        _AdChoicesLogo.GetComponent<RawImage>().texture = CTJ.Ads.Instance.GetAdChoicesLogo;
-        _Advertiser.GetComponent<Text>().text = CTJ.Ads.Instance.GetAdvertiser;
-        _Body.GetComponent<Text>().text = CTJ.Ads.Instance.GetBody;
-        _CallToAction.GetComponent<Text>().text = CTJ.Ads.Instance.GetCallToAction;
-        _Headline.GetComponent<Text>().text = CTJ.Ads.Instance.GetHeadline;
-        _Icon.GetComponent<RawImage>().texture = CTJ.Ads.Instance.GetIcon;
-        CTJ.Ads.Instance.RegisterAdChoicesLogo(_AdChoicesLogo);
-        CTJ.Ads.Instance.RegisterAdvertiser(_Advertiser);
-        CTJ.Ads.Instance.RegisterBody(_Body);
-        CTJ.Ads.Instance.RegisterCallToAction(_CallToAction);
-        CTJ.Ads.Instance.RegisterHeadline(_Headline);
-        CTJ.Ads.Instance.RegisterIcon(_Icon);
-        _NativeObject.SetActive(true);
     }
 }
